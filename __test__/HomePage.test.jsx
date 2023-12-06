ace57a2e89fe4153ea80d3118c381a5b305beee4
Chukwudobe Micah <dobemike101@gmail.com>
2023-12-06 19:22:49 +0100

    update

--- __test__/HomePage.test.jsx
+++ __test__/HomePage.test.jsx
@@ -1,30 +1,61 @@
 import Home from "@/pages";
-// import { render, expect } from "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe("Home Page Rendering", () => {
-  it("should show home page text", () => {
-    render(<Home />);
-    expect(screen.getByText("HOME PAGE")).toBeInTheDocument();
+describe("Home Page", () => {
+  describe("Rendering", () => {
+    it("should show home page text", () => {
+      render(<Home />);
+      expect(screen.getByText("HOME PAGE")).toBeInTheDocument();
+    });
+
+    it("should have button: Click me", () => {
+      render(<Home />);
+      expect(
+        screen.getByRole("button", { name: "click me" })
+      ).toBeInTheDocument();
+      render;
+    });
+
+    it("should have input", () => {
+      render(<Home />);
+      expect(screen.getByLabelText("Label text")).toBeInTheDocument();
+      // screen.getByLabelText("Label text");
+    });
+
+    it("should find placeholder input", () => {
+      render(<Home />);
+      // screen.getByPlaceholderText("search");
+      expect(screen.getByPlaceholderText("search")).toBeInTheDocument();
+    });
+
+    it("should find input by display value", () => {
+      render(<Home />);
+      screen.getByDisplayValue("seven");
+    });
+
+    it("should not find elemnt with show text", () => {
+      render(<Home />);
+      expect(screen.queryByText(/Show/)).not.toBeInTheDocument();
+    });
   });
 
-  it("should have button: Click me", () => {
-    render(<Home />);
-    expect(
-      screen.getByRole("button", { name: "click me" })
-    ).toBeInTheDocument();
-    render;
-  });
-
-  it("should have input", () => {
-    render(<Home />);
-    expect(screen.getByLabelText("Label text")).toBeInTheDocument();
-    // screen.getByLabelText("Label text");
-  });
-
-  it("should find placeholder input", () => {
-    render(<Home />);
-    // screen.getByPlaceholderText("search");
-    expect(screen.getByPlaceholderText("search")).toBeInTheDocument();
+  describe("Behaviour", () => {
+    it("should find show txt button and find new text", async () => {
+      render(<Home />);
+      expect(screen.queryByText("Show text")).not.toBeInTheDocument();
+      const showTextBtn = screen.getByRole("button", { name: "click me" });
+      await userEvent.click(showTextBtn);
+      // expect(screen.getByText("Show text")).toBeInTheDocument();
+      // expect(
+      //   await screen.findByText("Show text", {}, { timeout: 5000 })
+      // ).toBeInTheDocument()
+      await waitFor(
+        () => {
+          expect(screen.getByText("Show text")).toBeInTheDocument();
+        },
+        { timeout: 1200 }
+      );
+    });
   });
 });
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [showText, setShowText] = useState(false);
+  const toggleText = () => {
+    setShowText((prev) => !prev);
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <h1>HOME PAGE</h1>
-      <button>click me</button>
       <label htmlFor="input-text">Label text</label>
       <input id="input-text" />
       <input placeholder="search"></input>
+      <input value="seven"></input>
+      {showText && <div>Show text</div>}
+      <button onClick={() => setTimeout(toggleText, 100)}>click me</button>
     </main>
   );
 }
